edfbad1e50a359792b2a21323fc72a1d5cf74bc1
Nyame1997 <ciaravillanueva1997@gmail.com>
2021-04-12 09:06:52 -0400

    Add IEX gem and stock model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_010923) do
+ActiveRecord::Schema.define(version: 2021_04_12_130318) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
